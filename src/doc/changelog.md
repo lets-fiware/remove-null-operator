6e39f2c317039d2f02c211e54f4cdb2ce47b80b5
Kazuhito Suda <kazuhito@fisuda.jp>
2019-11-16 09:04:08 +0900

    Add feature that null replace with empty array or empty object

--- src/doc/changelog.md
+++ src/doc/changelog.md
@@ -1,7 +1,8 @@
-## v0.1.0 [Extra release info, e.g. release date]
+## v0.1.1
+
+- Add feature that null replace with empty array or empty object.
+
+
+## v0.1.0
 
 Initial version
-
-- change 1
-- change 2
-- ...
--- src/js/main.js
+++ src/js/main.js
@@ -10,8 +10,25 @@
 
     "use strict";
 
+    var mode = MashupPlatform.prefs.get('mode');
+
+    MashupPlatform.prefs.registerCallback(function (new_preferences) {
+        mode = MashupPlatform.prefs.get('mode');
+    }.bind(this));
+
     MashupPlatform.wiring.registerCallback('input', function (data) {
-        if (data !== null) {
+        if (data == null) {
+            switch (mode) {
+            case 'list':
+                data = [];
+                break;
+            case 'object':
+                data = {};
+                break;
+            }
+        }
+
+        if (data !== null || mode == 'pass') {
             MashupPlatform.wiring.pushEvent("output", data);
         }
     });
